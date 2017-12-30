a60b5210a328d2eb12f96c5efe391b79510f5e5e
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-12-30 14:11:02 -0500

    Update palendrome_finder.py

--- palendrome_finder.py
+++ palendrome_finder.py
@@ -4,6 +4,20 @@ from datetime import date
 
 count = 0
 
+def find_century(year):
+    if (year % 100) >= 50:
+        century_start = int(round(year, -2)) - 100
+        century_end = int(round(year, -2))
+    else:
+        century_start = int(round(year, -2))
+        century_end = int(round(year, -2)) + 100
+
+    print 'Searching for palindromes between the years {} and {}\n'.format(century_start, century_end)
+    start_date = datetime.now().date().replace(year=century_start, month=1, day=1)    
+    end_date = datetime.now().date().replace(year=century_end-1, month=12, day=31)
+    day_count = (end_date - start_date).days # Tracks the total amount of days in a given century
+    find_palindromes(start_date, day_count)
+
 def find_palindromes(start_date, day_count):
     for i in xrange(0,day_count):
         if start_date.month in range (1,10):
@@ -24,20 +38,6 @@ def find_palindromes(start_date, day_count):
         start_date += timedelta(days=1)
 
     print "Total number of Palindromes found: {}".format(count)
-
-def find_century(year):
-    if (year % 100) >= 50:
-        century_start = int(round(year, -2)) - 100
-        century_end = int(round(year, -2))
-    else:
-        century_start = int(round(year, -2))
-        century_end = int(round(year, -2)) + 100
-
-    print 'Searching for palindromes between the years {} and {}\n'.format(century_start, century_end)
-    start_date = datetime.now().date().replace(year=century_start, month=1, day=1)    
-    end_date = datetime.now().date().replace(year=century_end-1, month=12, day=31)
-    day_count = (end_date - start_date).days # Tracks the total amount of days in a given century
-    find_palindromes(start_date, day_count)
 
 # Only works with dates between 1000-9999
 # Can easily use exception handling to filter out invalid inputs
